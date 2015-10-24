e6060befa2f95b77a99fbd2e7108278ad57b35ba
thodep <thodep@yahoo.com>
2015-10-24 18:16:38 -0400

    Done lay out review VC

--- TorontoCheapEats/ReviewViewController.swift
+++ TorontoCheapEats/ReviewViewController.swift
@@ -9,11 +9,10 @@
 import UIKit
 import Parse // to save rating on Parse
 
-class ReviewViewController: UIViewController , UIAlertViewDelegate {
+class ReviewViewController: UIViewController , UIAlertViewDelegate, FloatRatingViewDelegate {
 
-    @IBOutlet weak var badRating: ButtonStyle!
-    
-    @IBOutlet weak var goodRating: ButtonStyle!
+ 
+    @IBOutlet weak var RatingView: FloatRatingView!
     
     @IBOutlet weak var closeButton: ButtonStyle!
     
@@ -27,6 +26,9 @@ class ReviewViewController: UIViewController , UIAlertViewDelegate {
     var isItCheap : Bool?
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Required float rating view params
+        self.RatingView.emptyImage = UIImage(named: "StarEmpty")
+        self.RatingView.fullImage = UIImage(named: "StarFull")
         
         backgroundImage.alpha = 0.4
         bgroundView.alpha = 0.91
@@ -51,64 +53,76 @@ class ReviewViewController: UIViewController , UIAlertViewDelegate {
         super.viewDidAppear(true)
     }
     
-    @IBAction func happButtonPressed(sender: AnyObject) {
-        let uiAlert = UIAlertController(title: "Awesome", message: "Thanks for your good rating", preferredStyle: UIAlertControllerStyle.Alert)
-        self.presentViewController(uiAlert, animated: true, completion: nil)
-
-        uiAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
-            print("Click of default button")
-            
-            // to go back to detailVC
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }))
-        
-        uiAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
-            print("Click of cancel button")
-        }))
-        isItCheap = true
-        cheapOrNot()
-    }
+//    @IBAction func happButtonPressed(sender: AnyObject) {
+//        let uiAlert = UIAlertController(title: "Awesome", message: "Thanks for your good rating", preferredStyle: UIAlertControllerStyle.Alert)
+//        self.presentViewController(uiAlert, animated: true, completion: nil)
+//
+//        uiAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+//            print("Click of default button")
+//            
+//            // to go back to detailVC
+//            self.dismissViewControllerAnimated(true, completion: nil)
+//        }))
+//        
+//        uiAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
+//            print("Click of cancel button")
+//        }))
+//        isItCheap = true
+//        cheapOrNot()
+//    }
     
     
-    @IBAction func unHappyButtonPressed(sender: AnyObject) {
-        let uiAlert = UIAlertController(title: "Great", message: "Thanks for your feedback", preferredStyle: UIAlertControllerStyle.Alert)
-        self.presentViewController(uiAlert, animated: true, completion: nil)
-        
-        uiAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
-            print("Click of default button")
-          
-             // to go back to detailVC
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }))
-        
-        uiAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
-            print("Click of cancel button")
-        }))
-        isItCheap = false
-        cheapOrNot()
-    }
+//    @IBAction func unHappyButtonPressed(sender: AnyObject) {
+//        let uiAlert = UIAlertController(title: "Great", message: "Thanks for your feedback", preferredStyle: UIAlertControllerStyle.Alert)
+//        self.presentViewController(uiAlert, animated: true, completion: nil)
+//        
+//        uiAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+//            print("Click of default button")
+//          
+//             // to go back to detailVC
+//            self.dismissViewControllerAnimated(true, completion: nil)
+//        }))
+//        
+//        uiAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
+//            print("Click of cancel button")
+//        }))
+//        isItCheap = false
+//        cheapOrNot()
+//    }
     // Function to save customer rating on Parse
-    func cheapOrNot(){
-        if let currentRest = currentResturant {
-            // Create a PF Class Object called CheapOrNotReview
-        let review = PFObject(className: "CheapOrNotReview")
+//    func cheapOrNot(){
+//        if let currentRest = currentResturant {
+//            // Create a PF Class Object called CheapOrNotReview
+//        let review = PFObject(className: "CheapOrNotReview")
+//    
+//          // We create cheap and userID objects into the CheapOrNotReview Class
+//        review["cheap"] = isItCheap
+//        review["restaurantName"] = currentRest.name
+//        review["userID"] =  PFUser.currentUser()
+//           // Save the PFObject in background
+//            review.saveInBackgroundWithBlock({ (success, error: NSError?) -> Void in
+//                if (error != nil ){
+//                 print(error)
+//                
+//                } else {
+//                    print("review added ")
+//                }
+//          })
+//        }
+//        
+//    }
+    // MARK: FloatRatingViewDelegate
     
-          // We create cheap and userID objects into the CheapOrNotReview Class
-        review["cheap"] = isItCheap
-        review["restaurantName"] = currentRest.name
-        review["userID"] =  PFUser.currentUser()
-           // Save the PFObject in background
-            review.saveInBackgroundWithBlock({ (success, error: NSError?) -> Void in
-                if (error != nil ){
-                 print(error)
-                
-                } else {
-                    print("review added ")
-                }
-          })
-        }
-        
+    func floatRatingView(ratingView: FloatRatingView, isUpdating rating:Float) {
+       // self.liveLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+       //print(FloatRatingView.touchesMoved(self))
     }
+    //
+    func floatRatingView(ratingView: FloatRatingView, didUpdate rating: Float) {
+       // self.updatedLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+    }
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
